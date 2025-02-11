d45bcabfca4109dcf819cf59b7b4e739357c2f71
dju4mota <fernando.b@ges.inatel.br>
2025-02-11 15:54:04 -0300

    Menu Start

---
+++ MontysRevenge/Assets/scripts/StartMenu.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button newGame;
+    public Button quitGame;
+    
+
+    private void Start()
+    {
+        newGame.onClick.AddListener(StartNewGame);    
+        quitGame.onClick.AddListener(Quit);    
+    }
+
+
+    private static void StartNewGame()
+    {
+        SceneManager.LoadScene("Juntando");
+    }
+
+    private static void Quit()
+    {
+        Application.Quit();
+    }
+    
+    
+}
